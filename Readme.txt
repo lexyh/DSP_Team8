631ee67c1e3e73dae60877268c61fdc8d887ce79
hayden puckeridge <hayden.d.puckeridge@student.uts.edu.au>
2021-10-27 21:05:20 +1100

    updated test_datetime to unittest method

--- Readme.txt
+++ Readme.txt
@@ -1,1 +1,6 @@
 initial set up repo for Assignment 8
+
+HP 27/10/21
+to run tests:
+python3 -m unittest src/test/test_datetime.py
+must be run from this directory
--- src/test/test_datetime.py
+++ src/test/test_datetime.py
@@ -1,19 +1,39 @@
 #!/bin/env python3
 # To be filled by students
-import datetime
+import unittest
+from src.datetime import DateColumn
 import pandas as pd
 
-# read csv
-df = pd.read_csv("01-01-2021_datetime_test.csv")
+class TestDateTime(unittest.TestCase):  
+    def test_datetime(self):
+        ### test on dummy data ###
+        # create series of data
+        dc1 = DateColumn()
+        dc1.col_name = "my_test"
+        dc1.serie = pd.to_datetime(pd.Series(["13:01:01 01/01/2021", "1:01:01 PM 01/01/2021", None, "13:01:02 01/03/2021", "2021/01/03 13:01:02", "13:01:02 13/1/2021" ]))
 
-# init DateColumn
-dc = datetime.DateColumn()
-dc.col_name = "Last_Update"
-dc.serie = df[dc.col_name]
+        # test methods
+        self.assertEqual(dc1.get_name(),"my_test")
+        self.assertEqual(dc1.get_unique(),3)
+        self.assertEqual(dc1.get_missing(),1)
+        
+        ### test on covid-19 sample csv ###
+        csv_path = "01-01-2021_test.csv"
+        
+        if csv_path:
+            # read csv
+            df = pd.read_csv(csv_path)
 
-# test methods
-print("get_name =",dc.get_name())
+            # init DateColumn
+            dc2 = DateColumn()
+            dc2.col_name = "Last_Update"
+            dc2.serie = pd.to_datetime(df[dc2.col_name])
 
-print("get_unique =",dc.get_unique())
+            # test methods
+            print("Outputs using file",csv_path)
+            print("get_name =",dc2.get_name())
+            print("get_unique =",dc2.get_unique())
+            print("get_missing =",dc2.get_missing())
 
-print("get_missing =",dc.get_missing())
+if __name__ == '__main__':
+    unittest.main()
