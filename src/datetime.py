e072d4f4fd5fbdb16e8dc2181419a73411a8e5ca
hayden puckeridge <hayden.d.puckeridge@student.uts.edu.au>
2021-10-31 11:32:56 +1100

    completed datetime module and testing scripts

--- src/datetime.py
+++ src/datetime.py
@@ -14,71 +14,95 @@ class DateColumn:
     """
     Return name of selected column
     """
-    
     return self.col_name
 
   def get_unique(self):
     """
-    Return number of unique values for selected column
+    Return number of unique values for selected column 
     """
+    # missing values are not included in the count of unique values
     return self.serie.dropna().unique().size
 
   def get_missing(self):
     """
     Return number of missing values for selected column
     """
+    # missing values are None or numpy.NaN
     return self.serie.isna().sum()
 
   def get_weekend(self):
     """
     Return number of occurrence of days falling during weekend (Saturday and Sunday)
     """
-    return None
+    # convert to a series of integers denoting weekday number, 0=Monday, 6=Sunday 
+    day_nums = self.serie.dt.weekday
+    
+    return sum((day_nums == 5) | (day_nums == 6))
 
   def get_weekday(self):
     """
     Return number of weekday days (not Saturday or Sunday)
     """
-    return None
+    # convert to a series of integers denoting weekday number, 0=Monday, 6=Sunday 
+    day_nums = self.serie.dt.weekday
+    
+    return sum((day_nums >= 0) & (day_nums <= 4))
   
   def get_future(self):
     """
     Return number of cases with future dates (after today)
     """
-    return None
+    # convert to a series of dates
+    dates = self.serie.apply(lambda x: x.date())
+    # get today's date
+    today_date = pd.Timestamp.today().date()
+    
+    return sum(dates > today_date)
 
   def get_empty_1900(self):
     """
     Return number of occurrence of 1900-01-01 value
     """
-    return None
+    return sum(self.serie == pd.Timestamp(1900, 1, 1))
 
   def get_empty_1970(self):
     """
     Return number of occurrence of 1970-01-01 value
     """
-    return None
+    return sum(self.serie == pd.Timestamp(1970, 1, 1))
 
   def get_min(self):
     """
     Return the minimum date
     """
-    return None
+    return min(self.serie)
 
   def get_max(self):
     """
     Return the maximum date 
     """
-    return None
+    return max(self.serie)
 
   def get_barchart(self):
-    """
+    """ 
     Return the generated bar chart for selected column
     """
-    return None
+    # get series of frequency value counts
+    df = self.serie.value_counts()
+    # plot bar chart
+    return st.bar_chart(df)
 
   def get_frequent(self):
     """
     Return the Pandas dataframe containing the occurrences and percentage of the top 20 most frequent values
     """
-    return None
+    # get series of frequency value counts and convert to a dataframe
+    df = pd.DataFrame(self.serie.value_counts()).reset_index()
+    # name the columns
+    df.columns = ["value", "occurrence"]
+    # get grand total of counts
+    total = df["occurrence"].sum()
+    # calculate percentage column as a decimal number
+    df["percentage"] = df["occurrence"] / total
+    # display dataframe as table
+    return st.dataframe(df)
--- src/test/test_datetime.py
+++ src/test/test_datetime.py
@@ -10,12 +10,19 @@ class TestDateTime(unittest.TestCase):
         # create series of data
         dc1 = DateColumn()
         dc1.col_name = "my_test"
-        dc1.serie = pd.to_datetime(pd.Series(["13:01:01 01/01/2021", "1:01:01 PM 01/01/2021", None, "13:01:02 01/03/2021", "2021/01/03 13:01:02", "13:01:02 13/1/2021" ]))
+        dc1.serie = pd.to_datetime(pd.Series(["13:01:01 01/01/2021", "1:01:01 PM 01/01/2021", None, "13:01:02 01/03/2021", "2021/01/03 13:01:02", "13:01:02 13/1/2021", "13:01:02 13/1/2022", "1900/01/01", "1970/01/01"]), dayfirst=True)
 
         # test methods
         self.assertEqual(dc1.get_name(),"my_test")
-        self.assertEqual(dc1.get_unique(),3)
+        self.assertEqual(dc1.get_unique(),7)
         self.assertEqual(dc1.get_missing(),1)
+        self.assertEqual(dc1.get_weekday(),7)
+        self.assertEqual(dc1.get_weekend(),1)
+        self.assertEqual(dc1.get_future(),1)
+        self.assertEqual(dc1.get_empty_1900(),1)
+        self.assertEqual(dc1.get_empty_1970(),1)
+        self.assertEqual(dc1.get_min(),pd.Timestamp("1900/01/01 00:00:00"))
+        self.assertEqual(dc1.get_max(),pd.Timestamp("2022/01/13 13:01:02"))
         
         ### test on covid-19 sample csv ###
         csv_path = "01-01-2021_test.csv"
@@ -27,13 +34,20 @@ class TestDateTime(unittest.TestCase):
             # init DateColumn
             dc2 = DateColumn()
             dc2.col_name = "Last_Update"
-            dc2.serie = pd.to_datetime(df[dc2.col_name])
+            dc2.serie = pd.to_datetime(df[dc2.col_name], dayfirst=True)
 
             # test methods
             print("Outputs using file",csv_path)
             print("get_name =",dc2.get_name())
             print("get_unique =",dc2.get_unique())
             print("get_missing =",dc2.get_missing())
+            print("get_weekend =",dc2.get_weekend())
+            print("get_weekday =",dc2.get_weekday())
+            print("get_future =",dc2.get_future())
+            print("get_empty_1900 =",dc2.get_empty_1900())
+            print("get_empty_1970 =",dc2.get_empty_1970())
+            print("get_min = ",dc2.get_min())
+            print("get_max = ",dc2.get_max())
 
 if __name__ == '__main__':
     unittest.main()
