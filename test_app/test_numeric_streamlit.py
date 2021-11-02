63312385cfb52e7d614e69caa3685bd7c8f5f38d
Anna <fillgap7@gmail.com>
2021-11-02 21:25:09 +1100

    update numeric.testapp

--- test_app/test_numeric_streamlit.py
+++ test_app/test_numeric_streamlit.py
@@ -5,15 +5,15 @@ import pandas as pd
 csv_path = "01-01-2021_test.csv"
 df = pd.read_csv(csv_path)
 
-# init DateColumn
-dc = dt.DateColumn()
+# init NumericColumn
+dc = dt.NumericColumn()
 dc.col_name = "Last_Update"
-dc.serie = pd.to_datetime(df[dc.col_name], dayfirst=True)
+dc.serie = pd.to_numeric(df[dc.col_name], dayfirst=True)
 
 # read csv
 df = pd.read_csv(csv_path)
 
-st.title("Datetime Test")
+st.title("Numeric Test")
 
 dc.get_barchart()
 dc.get_frequent()
