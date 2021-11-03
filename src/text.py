1182de179dba1c11baa3dbef28966b943b08b4f5
Lex <lexyhendo@gmail.com>
2021-11-04 08:24:28 +1100

    Finalising the text.py module
    Updated the descriptive comment, corrected an issue with returning the mode.

--- src/text.py
+++ src/text.py
@@ -5,128 +5,117 @@ import pandas as pd
 import plotly as py
 import plotly.graph_objs as go
 
-#Testing github desktop changes
+# Author: A Henderson. 
+# defining the TextColumn data class, imported as tc in the streamlit_app.py 
+# methods relating to the TextColumn are included.  
 
 @dataclass
 class TextColumn:
-  col_name: str = None
-  serie: pd.Series = None
-  
-  def __init__(self):
-    print('init')
-  
-  def get_name(self):
-    """
-    Return name of selected column
-    """
-    return self.col_name
+    col_name: str = None
+    serie: pd.Series = None
 
-  def get_unique(self):
-    """
-    Return number of unique values for selected column
-    """
-    return self.serie.nunique()
+    def __init__(self):
+        pass
 
-  def get_missing(self):
-    """
-    Return number of missing values for selected column
-    """
-    return self.serie.isna().sum()
+    def get_name(self):
+        """
+        Return name of selected column
+        """
+        return self.col_name
 
-  def get_empty(self):
-    """
-    Return number of rows with empty string for selected column
-    """
-    return self.serie.eq('').sum()
+    def get_unique(self):
+        """
+        Return number of unique values for selected column
+        """
+        return self.serie.nunique()
 
-  def get_whitespace(self):
-    """
-    Return number of rows with only whitespaces for selected column
-    """
-    return self.serie.str.isspace().sum()
+    def get_missing(self):
+        """
+        Return number of missing values for selected column
+        """
+        return self.serie.isna().sum()
 
-  def get_lowercase(self):
-    """
-    Return number of rows with only lower case characters for selected column
-    """
-    return self.serie.str.islower().sum()
+    def get_empty(self):
+        """
+        Return number of rows with empty string for selected column
+        """
+        return self.serie.eq('').sum()
 
-  def get_uppercase(self):
-    """
-    Return number of rows with only upper case characters for selected column
-    """
-    return self.serie.str.isupper().sum()
-  
-  def get_alphabet(self):
-    """
-    Return number of rows with only alphabet characters for selected column
-    """
-    return self.serie.str.isalpha().sum()
+    def get_whitespace(self):
+        """
+        Return number of rows with only whitespaces for selected column
+        """
+        return self.serie.str.isspace().sum()
 
-  def get_digit(self):
-    """
-    Return number of rows with only numbers as characters for selected column
-    Assumption here is numeric values only included. Ignores subscript and special characters
-    """
-    return self.serie.str.isnumeric().sum()
+    def get_lowercase(self):
+        """
+        Return number of rows with only lower case characters for selected column
+        """
+        return self.serie.str.islower().sum()
 
-  def get_mode(self):
-    """
-    Return the mode value for selected column
-    """
-    # identify the mode, nulls are dropped
-    modes = self.serie.mode(dropna = True)
+    def get_uppercase(self):
+        """
+        Return number of rows with only upper case characters for selected column
+        """
+        return self.serie.str.isupper().sum()
 
-    # determine the caption to show
-    caption_text = None #empty string 
-    if modes is None:
-        caption_text = "The selected column contains unique values (no mode found)"
-    if len(modes) > 1:
-        caption_text = "Multiple values in this column are equally most frequent."
-    
-    # put results into a table to improve presentation where multiple modes are found
-    
-    fig = go.Figure(data=[go.Table(
-    header=dict(
-            fill_color='blue',
-            align='center'),
-    cells=dict(values=modes['Mode Values'],
-            fill_color='white',
-            align='center'))
-    ])
+    def get_alphabet(self):
+        """
+        Return number of rows with only alphabet characters for selected column
+        """
+        return self.serie.str.isalpha().sum()
 
-    return fig
+    def get_digit(self):
+        """
+        Return number of rows with only numbers as characters for selected column
+        Assumption here is numeric values only included. Ignores subscript and special characters
+        """
+        return self.serie.str.isnumeric().sum()
+
+    def get_mode(self):
+        """
+        Return the mode value(s) for selected column
+        Multiple mode values may exist for a single column
+        
+        """
+        # identify the mode, nulls are dropped, and save it to a data frame (for improved presentation in streamlit)
+        modes = pd.DataFrame(self.serie.mode())
+        modes.columns = ["Mode Values"]
+
+        return modes
 
 
-  def get_barchart(self):
-    """
-    Return the generated bar chart for selected column
-    """
-    x = self.serie.unique()
-    y = self.serie.value_counts()
+    def get_barchart(self):
+        """
+        Return the generated bar chart for selected column
+        """
+        x = self.serie.unique()
+        y = self.serie.value_counts()
 
-    fig = go.Figure(data=[go.Bar(x=x, y=y, hovertext = ['some text' ])])
-    
-    # figure formatting
-    fig.update_layout(title = "Frequency of Column Values") #bargap = 0.3, couldn't adjust
+        fig = go.Figure(data=[go.Bar(x=x, y=y, hovertext = ['some text' ])])
 
-    return fig
+        # figure formatting
+        fig.update_layout(title = "Frequency of Column Values") #bargap = 0.3, couldn't adjust
 
-  def get_frequent(self):
-    """
-    Return the Pandas dataframe containing the occurrences and percentage of the top 20 most frequent values
-    """
-    d = self.serie
+        return fig
 
-    ## adjust content to remove nulls and empty strings (drop whitespace, '' and nulls)
-    d = d[(d !='') & d.str.isspace() == False].dropna()
-    
-    #create data frame to publish to streamlit
-    df = pd.DataFrame(d.value_counts()).reset_index()
-    df.columns = ["Value", "Occurance"]
- 
-    #total number of data points in column, after removing nulls
-    total = self.serie.size # overall size
-    total_adj = d.size #adjusted for whitespace, empty and nulls 
-    df['Percentage'] = (df['Occurance']/total_adj)*100
-    return df 
+    def get_frequent(self):
+        """
+        Return the Pandas dataframe containing the occurrences and percentage of the top 20 most frequent values
+        """
+        #relabel for ease
+        d = self.serie
+
+        ## adjust content to remove nulls and empty strings (drop whitespace, '' and nulls)
+        d = d[(d !='') & d.str.isspace() == False].dropna()
+
+        #create data frame to publish to streamlit
+        df = pd.DataFrame(d.value_counts()).reset_index()
+        df.columns = ["Value", "Occurance"]
+
+        #total number of data points in column, after removing nulls
+        total = self.serie.size # overall size
+        total_adj = d.size #adjusted for whitespace, empty and nulls 
+        df['Percentage'] = (df['Occurance']/total_adj)*100
+        
+        return df 
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,146 @@
 # To be filled by students
 import streamlit as st
 import pandas as pd
-import src.data1 as da
+import src.data as da
 import src.datetime as dt
+import src.numeric as nm
+import src.text as tx
 
 st.title("Data Explorer Tool")
 # read data
 uploaded_file = st.file_uploader("Choose a CSV file")
 if uploaded_file is not None:
-    df =da.Dataset()
-    df = pd.read_csv(uploaded_file)
+    # initialise Dataset object - this includes all data in the CSV
+    ds = da.Dataset()
+    ds.name = "my_dataset"
+    # pandas is used to read the uploaded binary file as a CSV, then stored to the df attribute of Dataset object ds
+    ds.df = pd.read_csv(uploaded_file)
+    
+    # display overall Dataset information
     st.title("1. Overall Information")
-    st.write(df)
+    st.write(ds.df)
     st.write("Name of Table: ", uploaded_file.name)
-   # rows = da.get_n_rows()
+    rows = ds.get_n_rows()
+    cols = ds.get_n_cols()
     
-    # st.write("Number of Rows: " +rows)
+    st.write("Number of Rows: " + rows)
+    st.write("Number of Columns: " + cols) 
     
-
+    number = st.slider('Select the numbers of rows to be displayed',0,int(rows),5)
+    st.write("Top Rows of Table")
+    st.dataframe(ds.get_head(number))
+    st.write("Bottom Rows of Table")
+    st.dataframe(ds.get_tail(number))
+    st.write("Random Sample Rows of Table")
+    st.dataframe(ds.get_sample(number))
+    ### fill in other display information with data.py functions ###
     
+    # get a dictionary of column data types
+    dtype_dict = ds.get_cols_dtype()
+    
+    # create counters for writing subheading
+    # used to write the relevamt subheading text to the app for each column loop
 
-# init DateColumn
-    dc = dt.DateColumn()
-    dc.col_name = "Last_Update"
-    dc.serie = pd.to_datetime(df[dc.col_name], dayfirst=True)
+    t = 0 #counter for to track the text column instance it is. 
+    d = 0
+    n = 0
 
-    # read csv
-    ##df = pd.read_csv(csv_path)
+    # loop through each column in the Dataset and display the information for the corrosponding data type
+    for column in ds.df:
+        # get the column data type
+        dtype = dtype_dict[column]
+        
+        # for numeric columns
+        if dtype == "int64" or dtype == "float64":
+            # initialise NumericColumn object
+            nc = nm.NumericColumn()
+            nc.col_name = column
+            nc.serie = ds.df[column]
+            ### fill in other display information with numeric.py functions ###
+            
+        # for text columns
+        elif dtype == "object": 
+            # update counter for text columns 
+            t = t+1
+            
+            # initialise TextColumn object
+            tc = tx.TextColumn()
+            tc.col_name = column
+            tc.serie = ds.df[column]
+            
+            ### display information with text.py functions ###
 
-    st.title("Datetime Test")
+            def text_summary(TextColumn):
+                """
+                Pass text column methods to column to return value counts
+                Compile data into a pandas dataframe & return
+                
+                Expected parameter: TextColumn() 
+                Class: defined in text.py
+                """
+                
+                summary = {}
+                summary["Missing Values"] = TextColumn.get_missing()
+                summary["Whitespace Values"] = TextColumn.get_whitespace()
+                summary["Unique Values"] = TextColumn.get_unique()
+                summary["Empty Values"] = TextColumn.get_empty()
+                summary["All Lowercase"] = TextColumn.get_lowercase()
+                summary["All Uppercase"] = TextColumn.get_uppercase()
+                summary["Only Alphabet Characters"] = TextColumn.get_alphabet()
+                summary["Only (numeric) Digits"] = TextColumn.get_digit()
+                
+                #convert to dataframe to allow streamlit to display the dictionary
+                
+                df = pd.DataFrame(pd.Series(summary).reset_index()) 
+                df.columns = ["Value Category", "Counts"]
+                
+                return df
 
-    dc.get_barchart()
-    dc.get_frequent()    
+            def mode_caption(md):
+
+                """
+                Logic to compile a caption to present underneath the mode for the column
+                """
+                caption_text = None #empty string 
+            
+                if md is None:
+                    caption_text = "No values shown as all values in the selected are unique"
+                if len(md) > 1 :
+                    caption_text = "Note: Multiple values in this column are equally most frequent."
+                    
+                return caption_text
+
+            # load in results for mode and subheading
+            subheader_text = (f'3.{t}. Field Name: {tc.col_name}') #subheading content
+            md = tc.get_mode() #return mode
+
+            # write to stremlit
+            st.subheader(tc.subheader_text)
+
+            st.write("The characteristics of this column are shown below:")
+                    
+            st.dataframe(text_summary(tc))
+
+            st.write("The most frequent values in this column are: ")
+            # logic to prepare caption to write under the 'mode' results. 
+            if md is None:
+                st.write('No Mode Found')
+            if md is not None:
+                st.write(md)
+            st.caption(mode_caption(md))
+
+            st.write('Frequency Table:')
+            st.dataframe(tc.get_frequent().style.highlight_max(axis=0)) #highlighting included but can be toggled off. 
+
+            st.write('Frequency Graph:')
+            st.plotly_chart(tc.get_barchart())
+
+
+        elif dtype == "datetime64":
+            # initialise DateColumn object
+            dc = dt.DateColumn()
+            dc.col_name = column
+            dc.serie = ds.df[column]
+            ### fill in other display information with datetime.py functions ###
+            dc.get_barchart()
+            dc.get_frequent()    
