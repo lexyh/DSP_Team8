aeb07feebe475eaa1422816673bc87b859d2d9a7
Lex <lexyhendo@gmail.com>
2021-11-04 11:08:29 +1100

    Update numeric.py
    Copied Anna's last script, but changed the get_histogram to return a plotly histogram object, not a native hist() object.

--- src/numeric.py
+++ src/numeric.py
@@ -1,83 +1,101 @@
-# To be filled by students
 import streamlit as st
 from dataclasses import dataclass
 import pandas as pd
+import numpy as np
+import matplotlib as mp
+import matplotlib.pyplot as plt
+import plotly.express as px
 
-
+#
 
 @dataclass
 class NumericColumn:
-  col_name: str
-  serie: pd.Series
- 
+  col_name: str = None
+  serie: pd.Series = None
+
   def get_name(self):
     """
     Return name of selected column
     """
-    return None
+    return self.col_name
 
   def get_unique(self):
     """
     Return number of unique values for selected column
     """
-    return None
+    return self.serie.dropna().unique.size
 
   def get_missing(self):
     """
     Return number of missing values for selected column
     """
-    return None
+    return self.serie.isna().sum()
 
   def get_zeros(self):
     """
     Return number of occurrence of 0 value for selected column
     """
-    return None
+    return ((self.serie==0).sum())
 
   def get_negatives(self):
     """
     Return number of negative values for selected column
     """
-    return None
+    return ((self.serie>=0).sum())
 
   def get_mean(self):
     """
     Return the average value for selected column
     """
-    return None
+    return self.serie.mean()
 
   def get_std(self):
     """
     Return the standard deviation value for selected column
     """
-    return None
+    return self.serie.std()
   
   def get_min(self):
     """
     Return the minimum value for selected column
     """
-    return None
+    return self.serie.min()
 
   def get_max(self):
     """
     Return the maximum value for selected column
     """
-    return None
+    return self.serie.max()
 
   def get_median(self):
     """
     Return the median value for selected column
     """
-    return None
+    return self.serie.median()
 
+
+  
   def get_histogram(self):
     """
     Return the generated histogram for selected column
+    A plotly obect is generated to improve the streamlit presentation
+
     """
-    return None
+    chart = px.histogram(self.serie, x = "Frequency of Column Values")
+    return chart
+
+ 
 
   def get_frequent(self):
     """
     Return the Pandas dataframe containing the occurrences and percentage of the top 20 most frequent values
     """
-    return None
+    df = pd.DataFrame(self.serie.value_counts()).reset_index()
+    # name the columns
+    df.columns = ["value", "occurrence"]
+    # get grand total of counts
+    total = df["occurrence"].sum()
+    # calculate percentage column as a decimal number
+    df["percentage"] = df["occurrence"] / total
+    # display dataframe as table
+    return st.dataframe(df)
