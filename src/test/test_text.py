4dd26882d9ff4d9f6a5166dddc0d772ce5ca165c
Lex <lexyhendo@gmail.com>
2021-11-05 12:56:15 +1100

    Update test_text.py
    Completed the unit testing script - confirm execution ok

--- src/test/test_text.py
+++ src/test/test_text.py
@@ -1,9 +1,11 @@
 # To be filled by students
+# Unit Testing for the TextColumn script. 
 import unittest
-from text import TextColumn
+from src.text import TextColumn
+import pandas as pd
 
 # read in dummy data to use as a test source.
-df = "text_test.csv"
+#df = "text_test.csv"
 
 # TextColumn() class defined with the following functions. 
 # get_name
@@ -20,36 +22,52 @@ df = "text_test.csv"
 # get_barchart
 
 class TestTextColumn(unittest.TestCase):
-    def colsetup(self):
-    #assigning values to attributes
-        self.col_name = "" 
-        self.serie = {}
-
-    
-    def test_str_empty(self):
-        # empty string value should return a false result
-        empty_code = ''
-        empty_response = check_valid_currency(empty_code)
-        self.assertEqual(empty_response, False)
-
-    def test_str_nonstrnum(self):
-        # => To be filled by student
-        # empty string value should return a false result
-        num_code = 42
-        num_response = check_valid_currency(num_code)
-        self.assertEqual(num_response, False) 
-
-    def test_str_nonstrlist(self):
-        # => To be filled by student
-        # empty string value should return a false result
-        list_code = ['AUD',"USD"]
-        list_response = check_valid_currency(list_code)
-        self.assertEqual(list_response, False) 
+    def test_attributes(self):
         
+        #test variables
+        col_empty_input = TextColumn()
+        col_empty_input.col_name = ""
+        col_empty_input.serie = pd.Series({})
+        col_test_unnamed = TextColumn()
+        
+        #Assignment as Expected where empty
+        self.assertEqual(col_empty_input.col_name, "") #object created even where empty data input
+        self.assertEqual(col_empty_input.serie, {}) # series attribute created even when no values
+        
+    def test_functions(self):
+            
+            #instantiate
+            col = TextColumn()
+            #create test object
+            col.serie = pd.Series(['IT WOULD','BE SO GOOD','BE SO GOOD','BE SO GOOD','be so good','true','false',0.65,0.58,'n/a','howierlkn',765765876, '%^&^%&^','',345,None,None,98,'   ',13434645])
+            col.col_name = "Column1"
+            
+            #expected values
+            n = 20
+            n_unique = 16
+            n_missing = 2 #note: raw csv seems to read in "n/a" as null. But writing it directly returns a string value.
+            n_empty = 1
+            n_whitespace = 1
+            n_lowercase = 4
+            n_uppercase = 4
+            n_alphabet = 8
+            n_digit = 4 #note only reads in full integers as numeric by the looks of it
+                            
+            #logic checks
+        
+            self.assertEqual(col.get_name(), "Column1")
+            self.assertEqual(col.get_unique(), n_unique)
+            self.assertEqual(col.get_missing(), n_missing)
+            self.assertEqual(col.get_empty(), n_empty)
+            self.assertEqual(col.get_whitespace(), n_whitespace)
+            self.assertEqual(col.get_lowercase(), n_lowercase)
+            self.assertEqual(col.get_uppercase(), n_uppercase)
+            self.assertEqual(col.get_alphabet(), n_alphabet)
+            self.assertEqual(col.get_digit(), n_digit)
+            self.assertTrue(isinstance(col.get_mode), list) #returned mode should be a list object
+            self.assertTrue(col.get_barchart() is not None)
+            self.assertTrue(col.get_frequent() is not None)
+            #get_frequent()
 
-    def test_str_emptylist(self):
-        # => To be filled by student
-        # empty string value should return a false result
-        em_list_code = []
-        em_list_response = check_valid_currency(em_list_code)
-        self.assertEqual(em_list_response, False) 
+if __name__ == '__main__':
+    unittest.main()
